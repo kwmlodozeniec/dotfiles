7d12ea92e39727872ed0ef7e7f82338298ac2765
kwmlodozeniec <1453791+kwmlodozeniec@users.noreply.github.com>
2021-03-05 12:12:33 +0000

    Powershell and Windows Terminal tweaks

--- powershell/profile.ps1
+++ powershell/profile.ps1
@@ -1,7 +1,16 @@
 $PSDefaultParameterValues["Out-File:Encoding"] = "utf8"
 $MaximumHistoryCount = 1000;
 
-Import-Module posh-git
+New-Alias ae .\env\Scripts\activate
+New-Alias de deactivate
+function git-short-log {& git log --pretty=oneline --abbrev-commit}
+New-Alias gsl git-short-log
+function git-rebase {& git rebase -i HEAD~$args}
+New-Alias gr git-rebase
+function git-push-force {& git push --force}
+New-Alias gpf git-push-force
+New-Alias vim nvim
+
 Import-Module oh-my-posh
-Set-PoshPrompt -Theme avit
+Set-PoshPrompt -Theme Agnoster
 Set-Location C:\repos
