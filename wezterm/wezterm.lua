97fa5908bf3e9e3fa60f9778bcbac74ecd3f003a
kwmlodozeniec <1453791+kwmlodozeniec@users.noreply.github.com>
2022-06-01 12:58:41 +0100

    Terminal config updates

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -2,22 +2,21 @@ local wezterm = require 'wezterm';
 
 local mykeys = {}
 for i = 1, 8 do
-  -- CTRL+ALT + number to activate that tab
-  table.insert(mykeys, {
-    key=tostring(i),
-    mods="ALT",
-    action=wezterm.action{ActivateTab=i-1},
-  })
+    -- CTRL+ALT + number to activate that tab
+    table.insert(mykeys, {
+        key = tostring(i),
+        mods = "ALT",
+        action = wezterm.action {
+            ActivateTab = i - 1
+        }
+    })
 end
 
 return {
-  font = wezterm.font_with_fallback({
-    "Fira Code",
-    "Symbols Nerd Font"
-  }),
-  font_size = 12,
-  color_scheme = "Gruvbox Dark",
-  use_fancy_tab_bar = true,
-  tab_max_width = 30,
-  keys = mykeys,
+    font = wezterm.font_with_fallback({"Cascadia Code", "Symbols Nerd Font"}),
+    font_size = 12,
+    color_scheme = "Gruvbox Dark",
+    use_fancy_tab_bar = true,
+    tab_max_width = 30,
+    keys = mykeys
 }
