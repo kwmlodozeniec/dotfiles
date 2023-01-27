8ac8afb83ab19d944e1cab804ad4b8475dd616d1
kwmlodozeniec <1453791+kwmlodozeniec@users.noreply.github.com>
2023-01-27 14:46:13 +0000

    Update zsh config

--- zsh/.zshenv
+++ zsh/.zshenv
@@ -31,6 +31,7 @@ export ARTIFACTORY_URL=""
 # Pyenv
 export PYENV_ROOT="$HOME/.pyenv"
 export PATH="$PYENV_ROOT/bin:$PATH"
+export PYENV_VIRTUALENV_DISABLE_PROMPT=1
 
 # NVM
 export NVM_DIR="$HOME/.nvm"
--- zsh/key_bindings.zsh
+++ zsh/key_bindings.zsh
@@ -1,3 +1,7 @@
+# [Ctrl-r] - Search backward incrementally for a specified string.
+# The string may begin with ^ to anchor the search to the beginning of the line.
+# bindkey '^r' history-incremental-search-backward
+
 # [Ctrl-Delete] - delete whole forward-word
 bindkey -M emacs '^[[3;5~' kill-word
 bindkey -M viins '^[[3;5~' kill-word
@@ -7,6 +11,7 @@ bindkey -M vicmd '^[[3;5~' kill-word
 bindkey -M emacs '^[[1;5C' forward-word
 bindkey -M viins '^[[1;5C' forward-word
 bindkey -M vicmd '^[[1;5C' forward-word
+
 # [Ctrl-LeftArrow] - move backward one word
 bindkey -M emacs '^[[1;5D' backward-word
 bindkey -M viins '^[[1;5D' backward-word
