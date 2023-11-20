f1ec04578d0df91572c37448e0c0ab8ec701c7fd
kwmlodozeniec <1453791+kwmlodozeniec@users.noreply.github.com>
2023-11-20 11:42:30 +0000

    nvim config update

--- nvim/.config/nvim/lua/custom/configs/null-ls.lua
+++ nvim/.config/nvim/lua/custom/configs/null-ls.lua
@@ -2,22 +2,23 @@ local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 local null_ls = require('null-ls')
 
 local opts = {
-    sources = {
-        null_ls.builtins.formatting.black, null_ls.builtins.diagnostics.mypy,
-        null_ls.builtins.diagnostics.ruff, null_ls.builtins.formatting.gofumpt,
-        null_ls.builtins.formatting.goimports_reviser,
-        null_ls.builtins.formatting.golines,
-        null_ls.builtins.formatting.prettierd,
-        null_ls.builtins.formatting.lua_format
-    },
+    sources = {null_ls.builtins.formatting.black, null_ls.builtins.diagnostics.ruff,
+               null_ls.builtins.formatting.gofumpt, null_ls.builtins.formatting.goimports_reviser,
+               null_ls.builtins.formatting.golines, null_ls.builtins.formatting.prettierd,
+               null_ls.builtins.formatting.lua_format},
     on_attach = function(client, bufnr)
         if client.supports_method("textDocument/formatting") then
-            vim.api.nvim_clear_autocmds({group = augroup, buffer = bufnr})
+            vim.api.nvim_clear_autocmds({
+                group = augroup,
+                buffer = bufnr
+            })
             vim.api.nvim_create_autocmd("BufWritePre", {
                 group = augroup,
                 buffer = bufnr,
                 callback = function()
-                    vim.lsp.buf.format({bufnr = bufnr})
+                    vim.lsp.buf.format({
+                        bufnr = bufnr
+                    })
                 end
             })
         end
