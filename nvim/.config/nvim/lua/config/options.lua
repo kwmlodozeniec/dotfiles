7313f4880607c0c11e2e13b010afc713327c3831
kwmlodozeniec <1453791+kwmlodozeniec@users.noreply.github.com>
2024-01-09 16:25:38 +0000

    Switched nvim config to lazygit

---
+++ nvim/.config/nvim/lua/config/options.lua
@@ -0,0 +1,7 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+vim.opt.winbar = "%=%m %f" -- Add file path to winbar
+vim.opt.relativenumber = false -- Disable relative line numbers
+-- Python config
+vim.g.python3_host_prog = "/home/krzysztof/.pyenv/versions/neovim3/bin/python"
